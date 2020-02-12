7f167353ada1e38f8e4f12ae9443b1ba1db2e63f
Seyed Modaresi <modaresi.mr@gmail.com>
2020-02-12 10:52:55 +0000

    Uploading the package

--- setup.py
+++ setup.py
@@ -5,28 +5,29 @@ import os
 from setuptools import setup, find_packages
 
 
-# Manually extract the __about__
-about = {}
-with open('pyprof_timer/__about__.py') as f:
-    exec(f.read(), about)
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name=about['__title__'],
-    version=about['__version__'],
-    author=about['__author__'],
-    author_email=about['__email__'],
-    maintainer=about['__author__'],
-    maintainer_email=about['__email__'],
-    keywords='Profiling, Timer, Python',
-    description=about['__doc__'],
-    license=about['__license__'],
-    long_description=about['__doc__'],
+    name='auto-profiler',
+    version=1,
+    description='A timer for profiling a Python function or snippet.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='modaresi mr',
+    author_email='modaresimr+git@gmail.com',
+    url="https://github.com/modaresimr/auto-profiler",
+    keywords='Profiling, Timer, Python, Auto prfiling, line profiler',
+    license='MIT',
     packages=find_packages(exclude=['tests']),
-    url=about['__uri__'],
     install_requires=[
         'monotonic>=1.3',
         'six>=1.10.0',
         'tree-format==0.1.1',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
